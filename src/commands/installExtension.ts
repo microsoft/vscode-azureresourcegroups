5b8eb3d5abef6b72286331e7b67dd228aae1b355
Alex Weininger <alex.weininger@live.com>
2022-03-29 17:20:48 -0700

    Show prompt before installing extension

--- src/commands/installExtension.ts
+++ src/commands/installExtension.ts
@@ -3,13 +3,33 @@
 *  Licensed under the MIT License. See License.txt in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
-import { IActionContext } from "@microsoft/vscode-azext-utils";
-import { commands } from "vscode";
+import { DialogResponses, IActionContext } from "@microsoft/vscode-azext-utils";
+import { commands, MessageItem, window } from "vscode";
+import { getAzureExtensions } from "../AzExtWrapper";
+import { ext } from "../extensionVariables";
+import { localize } from "../utils/localize";
 
 export async function installExtension(context: IActionContext, extensionId: string): Promise<void> {
-    context.errorHandling.suppressDisplay = true;
     context.telemetry.properties.extensionId = extensionId;
-    void commands.executeCommand('extension.open', extensionId);
-    await commands.executeCommand('workbench.extensions.installExtension', extensionId);
-    context.telemetry.properties.result = 'Succeeded';
+    await commands.executeCommand('extension.open', extensionId);
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const extension = getAzureExtensions().find(azExt => azExt.id === extensionId)!;
+    const installMessageItem: MessageItem = {
+        title: localize('installExtension', 'Install extension')
+    }
+    // Use extension-specific message later
+    const result = await window.showInformationMessage(localize('installExtension', "Install the '{0}' extension to enable additional features?", extension.label), {
+        modal: true
+    }, installMessageItem, DialogResponses.cancel);
+
+    if (result === installMessageItem) {
+        context.errorHandling.suppressDisplay = true;
+        // Will prompt if settings sync is enabled
+        await commands.executeCommand('workbench.extensions.installExtension', extensionId);
+        context.telemetry.properties.result = 'Succeeded';
+        void ext.tree.refresh(context);
+    } else {
+        context.telemetry.properties.result = 'Canceled';
+    }
 }
