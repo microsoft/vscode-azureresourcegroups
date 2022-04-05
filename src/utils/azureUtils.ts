67f146514b34dc517852ccc45e1150118660941f
Alex Weininger <alex.weininger@live.com>
2022-04-05 09:46:55 -0700

    Add display names and icons (#148)

--- src/utils/azureUtils.ts
+++ src/utils/azureUtils.ts
@@ -28,11 +28,16 @@ export function getResourceGroupFromId(id: string): string {
 export function createGroupConfigFromResource(resource: GenericResource, subscriptionId: string | undefined): GroupingConfig {
     const id = nonNullProp(resource, 'id');
     return {
-        resourceGroup: { keyLabel: 'Resource Groups', label: getResourceGroupFromId(id), id: id.substring(0, id.indexOf('/providers')) },
+        resourceGroup: {
+            keyLabel: 'Resource Groups',
+            label: getResourceGroupFromId(id),
+            id: id.substring(0, id.indexOf('/providers')).toLowerCase().replace('/resourcegroups', '/resourceGroups')
+        },
         resourceType: {
-            keyLabel: 'Resource Types', label: resource.type?.toLowerCase() ?? 'unknown',
-            id: `${subscriptionId}/${resource.type}` ?? 'unknown',
-            iconPath: getIconPath(resource?.type ?? 'resource')
+            keyLabel: 'Resource Types',
+            label: getName(resource) ?? resource.type ?? 'unknown',
+            id: getId(subscriptionId, resource.type, resource.kind),
+            iconPath: getIconPath(resource?.type ?? 'resource', resource.kind)
         },
         location: {
             id: `${subscriptionId}/${resource.location}` ?? 'unknown',
@@ -43,20 +48,16 @@ export function createGroupConfigFromResource(resource: GenericResource, subscri
     }
 }
 
-export function getIconPath(type?: string): TreeItemIconPath {
+function getId(subscriptionId?: string, type?: string, kind?: string): string {
+    const rType: string = getResourceType(type, kind);
+    return `${subscriptionId}/${rType}`;
+}
+
+export function getIconPath(type?: string, kind?: string): TreeItemIconPath {
     let iconName: string;
-    const rType: string | undefined = type?.toLowerCase();
-    if (rType && supportedIconTypes.includes(rType)) {
-        iconName = rType;
-        switch (rType) {
-            case 'microsoft.web/sites':
-                if (type?.toLowerCase().includes('functionapp')) {
-                    iconName = iconName.replace('sites', 'functionapp');
-                }
-                break;
-            default:
-        }
-        iconName = path.join('providers', iconName);
+    const rType: string = getResourceType(type, kind);
+    if (supportedIconTypes.includes(rType as SupportedTypes)) {
+        iconName = path.join('providers', rType);
     } else {
         iconName = 'resource';
     }
@@ -66,8 +67,8 @@ export function getIconPath(type?: string): TreeItemIconPath {
 
 
 // Execute `npm run listIcons` from root of repo to re-generate this list after adding an icon
-export const supportedIconTypes: string[] = [
-    'microsoft.web/functionapp',
+export const supportedIconTypes = [
+    'microsoft.web/sites/functionapp',
     'microsoft.web/hostingenvironments',
     'microsoft.web/kubeenvironments',
     'microsoft.web/serverfarms',
@@ -109,6 +110,7 @@ export const supportedIconTypes: string[] = [
     'microsoft.devtestlab/labs',
     'microsoft.devices/iothubs',
     'microsoft.dbforpostgresql/servers',
+    'microsoft.dbforpostgresql/flexibleservers',
     'microsoft.dbformysql/servers',
     'microsoft.containerservice/managedclusters',
     'microsoft.containerregistry/registries',
@@ -121,4 +123,77 @@ export const supportedIconTypes: string[] = [
     'microsoft.cache/redis',
     'microsoft.batch/batchaccounts',
     'microsoft.apimanagement/service',
-];
+] as const;
+
+type SupportedTypes = typeof supportedIconTypes[number];
+
+interface SupportedType {
+    displayName: string;
+}
+
+function getName(resource: GenericResource): string | undefined {
+    let type = resource.type?.toLowerCase();
+    if (isFunctionApp(resource.type, resource.kind)) {
+        type = 'microsoft.web/sites/functionapp';
+    }
+    if (type) {
+        return supportedTypes[type as SupportedTypes]?.displayName;
+    }
+    return undefined;
+}
+
+// intersect with Record<stirng, SupportedType> so we can add info for resources we don't have icons for
+type SupportedTypeMap = Partial<Record<SupportedTypes, SupportedType> & Record<string, SupportedType>>;
+
+const supportedTypes: SupportedTypeMap = {
+    'microsoft.web/sites': { displayName: localize('webApp', 'App Services') },
+    'microsoft.web/staticsites': { displayName: localize('staticWebApp', 'Static Web Apps') },
+    'microsoft.web/sites/functionapp': { displayName: localize('functionApp', 'Function App') },
+    'microsoft.compute/virtualmachines': { displayName: localize('virtualMachines', 'Virtual machines') },
+    'microsoft.storage/storageaccounts': { displayName: localize('storageAccounts', 'Storage accounts') },
+    'microsoft.network/networksecuritygroups': { displayName: localize('networkSecurityGroups', 'Network security groups') },
+    'microsoft.network/loadbalancers': { displayName: localize('loadBalancers', 'Load balancers') },
+    'microsoft.compute/disks': { displayName: localize('disks', 'Disks') },
+    'microsoft.compute/images': { displayName: localize('images', 'Images') },
+    'microsoft.compute/availabilitysets': { displayName: localize('availabilitySets', 'Availability sets') },
+    'microsoft.compute/virtualmachinescalesets': { displayName: localize('virtualMachineScaleSets', 'Virtual machine scale sets') },
+    'microsoft.network/virtualnetworks': { displayName: localize('virtualNetworks', 'Virtual networks') },
+    'microsoft.cdn/profiles': { displayName: localize('frontDoorAndcdnProfiles', 'Front Door and CDN profiles') },
+    'microsoft.network/publicipaddresses': { displayName: localize('publicIpAddresses', 'Public IP addresses') },
+    'microsoft.network/networkinterfaces': { displayName: localize('networkInterfaces', 'Network interfaces') },
+    'microsoft.network/networkwatchers': { displayName: localize('networkWatchers', 'Network watchers') },
+    'microsoft.batch/batchaccounts': { displayName: localize('batchAccounts', 'Batch accounts') },
+    'microsoft.containerregistry/registries': { displayName: localize('containerRegistry', 'Container registry') },
+    'microsoft.dbforpostgresql/servers': { displayName: localize('postgreSqlServers', 'PostgreSQL servers (Standard)') },
+    'microsoft.dbforpostgresql/flexibleservers': { displayName: localize('postgreSqlServers', 'PostgreSQL servers (Flexible)') },
+    'microsoft.dbformysql/servers': { displayName: localize('mysqlServers', 'MySql servers') },
+    'microsoft.sql/servers/databases': { displayName: localize('sqlDatabases', 'SQL databases') },
+    'microsoft.sql/servers': { displayName: localize('sqlServers', 'SQL servers') },
+    'microsoft.documentdb/databaseaccounts': { displayName: localize('documentDB', 'Azure CosmosDB') },
+    'microsoft.operationalinsights/workspaces': { displayName: localize('operationalInsightsWorkspaces', 'Operational Insights workspaces') },
+    'microsoft.operationsmanagement/solutions': { displayName: localize('operationsManagementSolutions', 'Operations management solutions') },
+    'microsoft.insights/components': { displayName: localize('insightsComponents', 'Application Insights') },
+    'microsoft.web/serverfarms': { displayName: localize('serverFarms', 'App Service plans') },
+    'microsoft.web/kubeenvironments': { displayName: localize('containerService', 'App Service Kubernetes Environment') },
+}
+
+export function isFunctionApp(type?: string, kind?: string): boolean {
+    if (type?.toLowerCase() === 'microsoft.web/sites') {
+        if (kind?.toLowerCase().includes('functionapp')) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function getRelevantKind(type?: string, kind?: string): string | undefined {
+    if (isFunctionApp(type, kind)) {
+        return 'functionapp';
+    }
+    return undefined;
+}
+
+function getResourceType(type?: string, kind?: string): string {
+    const relevantKind = getRelevantKind(type, kind);
+    return `${type?.toLowerCase()}${relevantKind ? `/${relevantKind}` : ''}`;
+}
