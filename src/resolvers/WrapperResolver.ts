7dd9872544999da77a629e8768e71e1b9abb8035
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2022-04-19 13:47:13 -0400

    Fix issues

--- src/resolvers/WrapperResolver.ts
+++ src/resolvers/WrapperResolver.ts
@@ -11,18 +11,19 @@ import { BuiltinResolver } from "./BuiltinResolver";
 
 /**
  * This resolver acts as a "placeholder" resolver when an extension is known for the resource type *and* installed (but not yet activated).
- * Upon the resolved node being clicked, it will reveal the resource as JSON.
+ * This returns a wrapper promise that will resolve when the resolver registers, with the value provided by that resolver
  */
 class WrapperResolver implements AppResourceResolver, BuiltinResolver {
     public readonly resolverKind = 'builtin';
 
-    public async resolveResource(subContext: ISubscriptionContext, resource: AppResource): Promise<ResolvedAppResourceBase> {
-        return new Promise<ResolvedAppResourceBase>((resolve) => {
+    public async resolveResource(subContext: ISubscriptionContext, resource: AppResource): Promise<ResolvedAppResourceBase | undefined | null> {
+        return new Promise<ResolvedAppResourceBase | undefined | null>((resolve) => {
             const disposable = ext.resolverRegisteredEmitter.event((resolver: AppResourceResolver) => {
-                disposable.dispose();
-
                 if (resolver.matchesResource(resource)) {
+                    disposable.dispose();
                     resolve(resolver.resolveResource(subContext, resource))
+                } else {
+                    // If it doesn't match; do nothing and also don't dispose of the event listener
                 }
             });
         });
