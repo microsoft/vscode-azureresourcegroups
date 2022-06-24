9cadd283d3e0cc9e33adbec8dc8c734df6a01dd3
phoff <phoff@microsoft.com>
2022-06-24 15:43:56 -0700

    Evict branch item for evicted child items.

--- src/tree/v2/ResourceGroupItemCache.ts
+++ src/tree/v2/ResourceGroupItemCache.ts
@@ -1,41 +1,49 @@
 import { ResourceGroupItem } from './ResourceGroupItem';
 
 export class ResourceGroupItemCache {
-    private readonly cache: Map<ResourceGroupItem, ResourceGroupItem[]> = new Map();
-    private readonly branchcache: Map<unknown, ResourceGroupItem> = new Map();
+    private readonly branchItemToItemCache: Map<unknown, ResourceGroupItem> = new Map();
+    private readonly itemToBranchItemCache: Map<ResourceGroupItem, unknown> = new Map();
+    private readonly itemToChildrenCache: Map<ResourceGroupItem, ResourceGroupItem[]> = new Map();
 
     addBranchItem(branchItem: unknown, item: ResourceGroupItem): void {
-        this.branchcache.set(branchItem, item);
+        this.branchItemToItemCache.set(branchItem, item);
+        this.itemToBranchItemCache.set(item, branchItem);
     }
 
     addItem(item: ResourceGroupItem, children: ResourceGroupItem[]): void {
-        this.cache.set(item, children);
+        this.itemToChildrenCache.set(item, children);
     }
 
     evictAll(): void {
-        this.branchcache.clear();
-        this.cache.clear();
+        this.branchItemToItemCache.clear();
+        this.itemToBranchItemCache.clear();
+        this.itemToChildrenCache.clear();
     }
 
     evictItemChildren(item: ResourceGroupItem): void {
-        // TODO: Evict any branch items associated with this item.
-
-        const children = this.cache.get(item);
+        const children = this.itemToChildrenCache.get(item);
 
         while (children?.pop()) {
-            const children = this.cache.get(item);
+            const children = this.itemToChildrenCache.get(item);
 
             children?.forEach(child => children.push(child));
 
-            this.cache.delete(item);
+            const branchItem = this.itemToBranchItemCache.get(item);
+
+            if (branchItem) {
+                this.branchItemToItemCache.delete(branchItem);
+            }
+
+            this.itemToBranchItemCache.delete(item);
+            this.itemToChildrenCache.delete(item);
         }
     }
 
     getItemForBranchItem(branchItem: unknown): ResourceGroupItem | undefined {
-        return this.branchcache.get(branchItem);
+        return this.branchItemToItemCache.get(branchItem);
     }
 
     updateItemChildren(item: ResourceGroupItem, children: ResourceGroupItem[]): void {
-        this.cache.set(item, children);
+        this.itemToChildrenCache.set(item, children);
     }
 }
