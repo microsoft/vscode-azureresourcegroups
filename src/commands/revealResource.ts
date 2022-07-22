eab46e514817ef781838db42bd59467d76477855
Alex Weininger <alex.weininger@live.com>
2022-07-22 08:50:33 -0700

    Make revealResource accept an Azure Id (#341)

--- src/commands/revealResource.ts
+++ src/commands/revealResource.ts
@@ -11,12 +11,16 @@ import { ext } from '../extensionVariables';
 import { AppResourceTreeItem } from '../tree/AppResourceTreeItem';
 import { SubscriptionTreeItem } from '../tree/SubscriptionTreeItem';
 
-export async function revealResource(context: IActionContext, resource: AppResource): Promise<void> {
-    context.telemetry.properties.resourceType = resource.type?.replace(/\//g, '|'); // Replace the slashes otherwise this gets redacted because it looks like a user file path
-    context.telemetry.properties.resourceKind = resource.kind;
+export async function revealResource(context: IActionContext, resourceId: string): Promise<void>;
+export async function revealResource(context: IActionContext, resource: AppResource): Promise<void>;
+export async function revealResource(context: IActionContext, arg: AppResource | string): Promise<void> {
+    const resourceId = typeof arg === 'string' ? arg : arg.id;
+
+    context.telemetry.properties.resourceType = parseAzureResourceId(resourceId).provider.replace(/\//g, '|');
+
     try {
-        const subscriptionNode: SubscriptionTreeItem | undefined = await ext.appResourceTree.findTreeItem(`/subscriptions/${parseAzureResourceId(resource.id).subscriptionId}`, { ...context, loadAll: true });
-        const appResourceNode: AppResourceTreeItem | undefined = await subscriptionNode?.findAppResourceByResourceId(context, resource.id);
+        const subscriptionNode: SubscriptionTreeItem | undefined = await ext.appResourceTree.findTreeItem(`/subscriptions/${parseAzureResourceId(resourceId).subscriptionId}`, { ...context, loadAll: true });
+        const appResourceNode: AppResourceTreeItem | undefined = await subscriptionNode?.findAppResourceByResourceId(context, resourceId);
         if (appResourceNode) {
             // ensure the parent node loaded this AppResourceTreeItem
             await appResourceNode.parent?.getCachedChildren(context);
@@ -25,5 +29,4 @@ export async function revealResource(context: IActionContext, resource: AppResou
     } catch (error) {
         context.telemetry.properties.revealError = parseError(error).message;
     }
-
 }
