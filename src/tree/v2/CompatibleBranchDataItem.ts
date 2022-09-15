b4381220be0ee2b4ecd15b727154687b79e01122
Alex Weininger <alex.weininger@live.com>
2022-09-14 19:51:48 -0700

    Add resource getter

--- src/tree/v2/CompatibleBranchDataItem.ts
+++ src/tree/v2/CompatibleBranchDataItem.ts
@@ -15,6 +15,11 @@ export class CompatibleBranchDataItem implements ResourceGroupsItem, Box, Compat
         itemCache.addBranchItem(this.branchItem, this);
     }
 
+    /** Needed for tree item picker PickAppResourceStep */
+    public get resource(): ApplicationResource | undefined {
+        return (this.branchItem as { resource?: ApplicationResource }).resource;
+    }
+
     async getChildren(): Promise<ResourceGroupsItem[] | undefined> {
         const children = await this.branchDataProvider.getChildren(this.branchItem);
         const factory = createBranchDataItemFactory(this.itemCache);
