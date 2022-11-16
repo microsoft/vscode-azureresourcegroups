4867558417cda71fc5898f06384d943ac377efd9
Alex Weininger <alex.weininger@live.com>
2022-11-16 14:07:47 -0800

    Add telemetry event for register application resource resolver

--- src/api/registerApplicationResourceResolver.ts
+++ src/api/registerApplicationResourceResolver.ts
@@ -3,7 +3,7 @@
 *  Licensed under the MIT License. See License.txt in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
-import { AzExtResourceType, AzExtTreeItem } from "@microsoft/vscode-azext-utils";
+import { AzExtResourceType, AzExtTreeItem, callWithTelemetryAndErrorHandlingSync } from "@microsoft/vscode-azext-utils";
 import { AppResourceResolver } from "@microsoft/vscode-azext-utils/hostapi";
 import { Disposable } from "vscode";
 import { ext } from "../extensionVariables";
@@ -13,18 +13,20 @@ import { ApplicationResourceBranchDataProvider } from "./v2/v2AzureResourcesApi"
 export const applicationResourceResolvers: Partial<Record<AzExtResourceType, AppResourceResolver>> = {};
 
 export function registerApplicationResourceResolver(type: AzExtResourceType, resolver: AppResourceResolver): Disposable {
-    if (applicationResourceResolvers[type]) {
-        throw new Error(`Application resource resolver with id '${type}' has already been registered.`);
-    }
+    return callWithTelemetryAndErrorHandlingSync('registerApplicationResourceProvider', () => {
+        if (applicationResourceResolvers[type]) {
+            throw new Error(`Application resource resolver with id '${type}' has already been registered.`);
+        }
 
-    applicationResourceResolvers[type] = resolver;
-    ext.emitters.onDidRegisterResolver.fire(resolver);
+        applicationResourceResolvers[type] = resolver;
+        ext.emitters.onDidRegisterResolver.fire(resolver);
 
-    const compat = new CompatibleApplicationResourceBranchDataProvider(resolver, 'azureResourceGroups.loadMore' /** TODO: what is the correct value for this? */);
+        const compat = new CompatibleApplicationResourceBranchDataProvider(resolver, 'azureResourceGroups.loadMore' /** TODO: what is the correct value for this? */);
 
-    ext.v2.api.registerApplicationResourceBranchDataProvider(type, compat as unknown as ApplicationResourceBranchDataProvider<AzExtTreeItem>);
+        ext.v2.api.registerApplicationResourceBranchDataProvider(type, compat as unknown as ApplicationResourceBranchDataProvider<AzExtTreeItem>);
 
-    return new Disposable(() => {
-        delete applicationResourceResolvers[type];
-    });
+        return new Disposable(() => {
+            delete applicationResourceResolvers[type];
+        });
+    }) as Disposable;
 }
