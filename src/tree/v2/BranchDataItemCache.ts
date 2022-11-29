867775478d7f29c39801df3e992d05dac4b3ec59
Alex Weininger <alex.weininger@live.com>
2022-11-29 10:03:55 -0800

    Remove `itemToBranchItemCache`

--- src/tree/v2/BranchDataItemCache.ts
+++ src/tree/v2/BranchDataItemCache.ts
@@ -7,16 +7,13 @@ import { ResourceGroupsItem } from './ResourceGroupsItem';
 
 export class BranchDataItemCache {
     private readonly branchItemToItemCache: Map<unknown, ResourceGroupsItem> = new Map();
-    private readonly itemToBranchItemCache: Map<ResourceGroupsItem, unknown> = new Map();
 
     addBranchItem(branchItem: unknown, item: ResourceGroupsItem): void {
         this.branchItemToItemCache.set(branchItem, item);
-        this.itemToBranchItemCache.set(item, branchItem);
     }
 
     evictAll(): void {
         this.branchItemToItemCache.clear();
-        this.itemToBranchItemCache.clear();
     }
 
     getItemForBranchItem(branchItem: unknown): ResourceGroupsItem | undefined {
