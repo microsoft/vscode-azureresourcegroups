d30cff53601e255ff615fbea63256603e50d1634
Megan Mott <meganmott@microsoft.com>
2023-01-23 13:48:17 -0800

    Add changes from third PR

--- src/extension.ts
+++ src/extension.ts
@@ -94,8 +94,6 @@ export async function activateInternal(context: vscode.ExtensionContext, perfSta
         registerApplicationResourceResolver('vscode-azureresourcegroups.wrapperResolver', wrapperResolver);
         registerApplicationResourceResolver('vscode-azureresourcegroups.installableAppResourceResolver', installableAppResourceResolver);
         registerApplicationResourceResolver('vscode-azureresourcegroups.shallowResourceResolver', shallowResourceResolver);
-
-        //await vscode.commands.executeCommand('setContext', 'azure-account.signedIn', await ext.rootAccountTreeItem.getIsLoggedIn());
     });
 
     return createApiProvider([
