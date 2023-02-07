25e6dc2a0da13ce0bd80320cd25a17d769ddece3
Alex Weininger <alex.weininger@live.com>
2023-02-07 13:39:52 -0800

    Report if extension was installed via command

--- src/commands/installExtension.ts
+++ src/commands/installExtension.ts
@@ -4,9 +4,37 @@
 *--------------------------------------------------------------------------------------------*/
 
 import { IActionContext } from "@microsoft/vscode-azext-utils";
-import { commands } from "vscode";
+import { commands, extensions } from "vscode";
 
 export async function installExtension(context: IActionContext, extensionId: string): Promise<void> {
     context.telemetry.properties.extensionId = extensionId;
     await commands.executeCommand('extension.open', extensionId);
+
+    function isInstalled(): boolean {
+        return !!extensions.getExtension(extensionId);
+    }
+
+    const alreadyInstalled = isInstalled();
+    context.telemetry.properties.alreadyInstalled = String(alreadyInstalled);
+
+    if (!alreadyInstalled) {
+        return new Promise((resolve) => {
+            const disposable = extensions.onDidChange(() => {
+                const installed = isInstalled();
+                context.telemetry.properties.installedAfter30s = String(isInstalled());
+
+                if (installed) {
+                    disposable.dispose();
+                    clearTimeout(timeout);
+                    resolve();
+                }
+            });
+
+            const timeout = setTimeout(() => {
+                context.telemetry.properties.installedAfter30s = 'false';
+                disposable.dispose();
+                resolve();
+            }, 1000 * 30 /* 30 seconds */);
+        });
+    }
 }
