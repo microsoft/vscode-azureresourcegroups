75847ac2b0d8e2534f442c652978467f116cca04
Alex Weininger <alex.weininger@live.com>
2023-02-08 11:03:28 -0800

    Report if extension was installed via command (#552)
    * Report if extension was installed via command
    
    * Change timeout to 1 minute
    
    * Make code clearer
    
    * Add comment

--- src/commands/installExtension.ts
+++ src/commands/installExtension.ts
@@ -4,9 +4,37 @@
 *--------------------------------------------------------------------------------------------*/
 
 import { IActionContext } from "@microsoft/vscode-azext-utils";
-import { commands } from "vscode";
+import { commands, extensions } from "vscode";
 
 export async function installExtension(context: IActionContext, extensionId: string): Promise<void> {
     context.telemetry.properties.extensionId = extensionId;
     await commands.executeCommand('extension.open', extensionId);
+
+    function isInstalled(): boolean {
+        return !!extensions.getExtension(extensionId);
+    }
+
+    const alreadyInstalled = isInstalled();
+    context.telemetry.properties.alreadyInstalled = String(alreadyInstalled);
+
+    if (!alreadyInstalled) {
+        context.telemetry.properties.installedAfter1m = 'false';
+        return new Promise((resolve) => {
+            const disposable = extensions.onDidChange(() => {
+                if (isInstalled()) {
+                    context.telemetry.properties.installedAfter1m = 'true';
+                    disposable.dispose();
+                    clearTimeout(timeout);
+                    resolve();
+                }
+            });
+
+            // Listen for extension change events for 1 minute at most.
+            // If VS Code is closed before this timeout, the event for this command isn't sent.
+            const timeout = setTimeout(() => {
+                disposable.dispose();
+                resolve();
+            }, 1000 * 60 * 1 /* 1 minute */);
+        });
+    }
 }
