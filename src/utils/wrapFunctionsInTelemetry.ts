aa6f6353b5e6a2ef2cbbad68a3b07aeba343eae1
Alex Weininger <alex.weininger@live.com>
2023-02-08 19:06:19 -0800

    Add wrapFunctionsInTelemetrySync function and add tests

--- src/utils/wrapFunctionsInTelemetry.ts
+++ src/utils/wrapFunctionsInTelemetry.ts
@@ -3,7 +3,7 @@
 *  Licensed under the MIT License. See License.txt in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
-import { callWithTelemetryAndErrorHandling, IActionContext } from "@microsoft/vscode-azext-utils";
+import { callWithTelemetryAndErrorHandling, callWithTelemetryAndErrorHandlingSync, IActionContext } from "@microsoft/vscode-azext-utils";
 
 interface WrapFunctionsInTelemetryOptions {
     /**
@@ -16,8 +16,15 @@ interface WrapFunctionsInTelemetryOptions {
     callbackIdPrefix?: string;
 }
 
+type AsyncFunctions<T extends Record<string, (...args: unknown[]) => unknown | Promise<unknown>>> = {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    [P in keyof T]: AsyncFunction<T[P]>;
+};
+
+type AsyncFunction<T extends (...args: unknown[]) => Promise<unknown> | unknown> = (...args: Parameters<T>) => ReturnType<T> extends Promise<infer TPromise> ? Promise<TPromise> : Promise<ReturnType<T>>;
+
 /**
- * Wraps a set of functions in telemetry and error handling.
+ * Wraps a set of functions in telemetry and error handling. Returned functions are always async.
  *
  * Automatically sets the following on context:
  * ```
@@ -26,18 +33,45 @@ interface WrapFunctionsInTelemetryOptions {
  * context.errorHandling.suppressReportIssue = true;
  * ```
  */
-export function wrapFunctionsInTelemetry<TFunctions extends Record<string, (...args: unknown[]) => unknown | Promise<unknown>>>(functions: TFunctions, options?: WrapFunctionsInTelemetryOptions): TFunctions {
+export function wrapFunctionsInTelemetry<TFunctions extends Record<string, (...args: unknown[]) => unknown | Promise<unknown>>>(functions: TFunctions, options?: WrapFunctionsInTelemetryOptions): AsyncFunctions<TFunctions> {
     const wrappedFunctions = {};
 
     Object.entries(functions).forEach(([functionName, func]) => {
-        wrappedFunctions[functionName] = async (...args: Parameters<typeof func>): Promise<ReturnType<typeof func>> => {
-            return await callWithTelemetryAndErrorHandling((options?.callbackIdPrefix ?? '') + functionName, async (context) => {
+        wrappedFunctions[functionName] = (...args: Parameters<typeof func>): ReturnType<typeof func> => {
+            return callWithTelemetryAndErrorHandling((options?.callbackIdPrefix ?? '') + functionName, async (context) => {
                 context.errorHandling.rethrow = true;
                 context.errorHandling.suppressDisplay = true;
                 context.errorHandling.suppressReportIssue = true;
                 options?.beforeHook?.(context);
-                // await to ensure errors are handled in this scope
                 return await func(...args);
+            });
+        }
+    });
+
+    return wrappedFunctions as AsyncFunctions<TFunctions>;
+}
+
+/**
+ * Wraps a set of sync functions in telemetry and error handling.
+ *
+ * Automatically sets the following on context:
+ * ```
+ * context.errorHandling.rethrow = true;
+ * context.errorHandling.suppressDisplay = true;
+ * context.errorHandling.suppressReportIssue = true;
+ * ```
+ */
+export function wrapFunctionsInTelemetrySync<TFunctions extends Record<string, (...args: unknown[]) => unknown>>(functions: TFunctions, options?: WrapFunctionsInTelemetryOptions): TFunctions {
+    const wrappedFunctions = {};
+
+    Object.entries(functions).forEach(([functionName, func]) => {
+        wrappedFunctions[functionName] = (...args: Parameters<typeof func>): ReturnType<typeof func> => {
+            return callWithTelemetryAndErrorHandlingSync((options?.callbackIdPrefix ?? '') + functionName, (context) => {
+                context.errorHandling.rethrow = true;
+                context.errorHandling.suppressDisplay = true;
+                context.errorHandling.suppressReportIssue = true;
+                options?.beforeHook?.(context);
+                return func(...args);
             });
         }
     });
