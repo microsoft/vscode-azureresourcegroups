460f0d2c0f0faa9b8650747b0b8a299be6b02c67
Alex Weininger <alex.weininger@live.com>
2023-02-17 18:42:54 -0800

    Fixup

--- extension.bundle.ts
+++ extension.bundle.ts
@@ -15,22 +15,27 @@
 // The tests should import '../extension.bundle'. At design-time they live in tests/ and so will pick up this file (extension.bundle.ts).
 // At runtime the tests live in dist/tests and will therefore pick up the main webpack bundle at dist/extension.bundle.js.
 export * from '@microsoft/vscode-azext-utils';
-export * from './api/src';
 export * from './api/src/AzExtResourceType';
+// export * from './api/src';
+export * from './api/src/extensionApi';
+export * from './api/src/resources/azure';
+export * from './api/src/resources/base';
+export * from './api/src/resources/workspace';
+export * from './api/src/utils/getApi';
+export * from './api/src/utils/wrapper';
 export * from './src/api/DefaultAzureResourceProvider';
 export * from './src/AzureService';
-export * from './src/commands/tags/getTagDiagnostics';
-export * from './src/utils/wrapFunctionsInTelemetry';
-export * from './src/tree/azure/GroupingItem';
-export * from './src/tree/azure/SubscriptionItem';
 export * from './src/commands/openInPortal';
+export * from './src/commands/tags/getTagDiagnostics';
 export * from './src/commands/viewProperties';
-export * from './api/src/utils/wrapper';
 // Export activate/deactivate for main.js
 export { activateInternal, deactivateInternal } from './src/extension';
 export * from './src/extensionVariables';
 export * from './src/tree/azure/AzureResourceItem';
+export * from './src/tree/azure/GroupingItem';
+export * from './src/tree/azure/SubscriptionItem';
 export * from './src/tree/BranchDataProviderItem';
 export * from './src/tree/InvalidItem';
+export * from './src/utils/wrapFunctionsInTelemetry';
 
 // NOTE: The auto-fix action "source.organizeImports" does weird things with this file, but there doesn't seem to be a way to disable it on a per-file basis so we'll just let it happen
