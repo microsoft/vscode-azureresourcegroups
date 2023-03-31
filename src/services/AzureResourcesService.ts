f3107209fae6a829a4f104daaae3b8ecb46f0f65
Alex Weininger <alex.weininger@live.com>
2023-03-31 15:36:25 -0400

    Make Azure resource provider mockable

---
+++ src/services/AzureResourcesService.ts
@@ -0,0 +1,31 @@
+import type { GenericResource, ResourceGroup, ResourceManagementClient } from "@azure/arm-resources";
+import { uiUtils } from "@microsoft/vscode-azext-azureutils";
+import { createSubscriptionContext, IActionContext } from "@microsoft/vscode-azext-utils";
+import { AzureSubscription } from "api/src/resources/azure";
+import { createResourceClient } from "../utils/azureClients";
+
+export interface AzureResourcesService {
+    listResources(context: IActionContext, subscription: AzureSubscription): Promise<GenericResource[]>;
+    listResourceGroups(context: IActionContext, subscription: AzureSubscription): Promise<ResourceGroup[]>;
+}
+
+export const defaultAzureResourcesServiceFactory = (): AzureResourcesService => {
+    async function createClient(context: IActionContext, subscription: AzureSubscription): Promise<ResourceManagementClient> {
+        const subContext = createSubscriptionContext(subscription);
+        return await createResourceClient([context, subContext]);
+    }
+    return {
+        async listResources(context: IActionContext, subscription: AzureSubscription): Promise<GenericResource[]> {
+            const client = await createClient(context, subscription);
+            return uiUtils.listAllIterator(client.resources.list());
+        },
+        async listResourceGroups(context: IActionContext, subscription: AzureSubscription): Promise<ResourceGroup[]> {
+            const client = await createClient(context, subscription);
+            return uiUtils.listAllIterator(client.resourceGroups.list());
+        },
+    }
+}
+
+export function getAzureResourcesService(): AzureResourcesService {
+    return defaultAzureResourcesServiceFactory();
+}
