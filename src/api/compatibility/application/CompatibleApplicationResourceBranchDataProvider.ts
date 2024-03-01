a736a812b9a3f8959cb36f341275c8a91182b80c
Nathan Turinski <naturins@microsoft.com>
2024-03-01 11:50:09 -0800

    outputChannel appendLog

--- src/api/compatibility/application/CompatibleApplicationResourceBranchDataProvider.ts
+++ src/api/compatibility/application/CompatibleApplicationResourceBranchDataProvider.ts
@@ -7,6 +7,7 @@ import { AzExtTreeItem, createSubscriptionContext, ISubscriptionContext } from '
 import type { AppResource, AppResourceResolver } from '@microsoft/vscode-azext-utils/hostapi';
 import { l10n } from 'vscode';
 import type { AzureResource, ResourceModelBase } from '../../../../api/src/index';
+import { ext } from '../../../extensionVariables';
 import { CompatibleBranchDataProviderBase } from '../CompatibleBranchDataProviderBase';
 import { CompatibleResolvedApplicationResourceTreeItem } from './CompatibleApplicationResourceTreeItem';
 
@@ -29,6 +30,7 @@ export class CompatibleApplicationResourceBranchDataProvider<TResource extends A
         const resolved = await this.resolver.resolveResource(subscriptionContext, oldAppResource);
         if (!resolved) {
             const noResolveError = l10n.t('Could not resolve resource "{0}"', element.id);
+            ext.outputChannel.appendLog(noResolveError);
             throw new Error(noResolveError);
         }
         const result = CompatibleResolvedApplicationResourceTreeItem.Create(element, resolved, subscriptionContext, this, element) as unknown as TModel;
