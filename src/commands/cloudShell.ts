7e38204105adb240c6d53a3e0cb1b168f8d06c1f
alexweininger <alex.weininger@live.com>
2024-04-09 14:29:31 -0400

    Finalize cloud shell command

--- src/commands/cloudShell.ts
+++ src/commands/cloudShell.ts
@@ -10,8 +10,9 @@ import { ext } from '../extensionVariables';
 
 // This command is meant to be called once vscode.dev connects to VS Code server running in Cloud Shell via Dev Tunnel.
 // As long as we're connected, it periodically does the following:
-//  1. refreshes the dev tunnel access token when connected to VS Code Server running in Cloud Shell
+//  1. refreshes the dev tunnel access token when connected for the VS Code Server
 //  2. calls the Cloud Shell /keepAlive endpoint to ensure Cloud Shell stays active
+//  3. calls the Cloud Shell /size endpoint to ensure the terminal stays active
 
 function delay(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms))
@@ -19,16 +20,29 @@ function delay(ms: number) {
 
 interface MaintainCloudShellConnectionOptions {
     consoleUri: string;
+    terminalId: string;
 }
 
 export async function maintainCloudShellConnection(_context: IActionContext, options: MaintainCloudShellConnectionOptions) {
     while (true) {
         try {
             await refreshDevTunnelAccessToken();
+        } catch (e) {
+            ext.outputChannel.error(parseError(e).message);
+        }
+
+        try {
             await cloudShellKeepAlive(options.consoleUri);
         } catch (e) {
             ext.outputChannel.error(parseError(e).message);
         }
+
+        try {
+            await cloudShellSize(options.consoleUri, options.terminalId);
+        } catch (e) {
+            ext.outputChannel.error(parseError(e).message);
+        }
+
         await delay(1000 * 60 * 10 /* 10 minutes */);
     }
 }
@@ -39,7 +53,7 @@ export async function refreshDevTunnelAccessToken() {
     if (!session) {
         ext.outputChannel.error('Failed to refresh Dev Tunnel access token.');
     } else {
-        ext.outputChannel.appendLog('Running tunnel user login...');
+        ext.outputChannel.debug('Running tunnel user login...');
         const codeCli = 'vscode';
         exec(`${codeCli} tunnel user login --provider microsoft --access-token ${session.accessToken}`,
             (error, stdout, stderr) => {
@@ -48,38 +62,63 @@ export async function refreshDevTunnelAccessToken() {
                     ext.outputChannel.error(stderr);
                     ext.outputChannel.error(stdout);
                 }
-                ext.outputChannel.appendLine('Successfully ran tunnel user login.');
+                ext.outputChannel.debug('Successfully ran tunnel user login.');
             }
         );
     }
 }
 
 async function cloudShellKeepAlive(consoleUri: string) {
-    // POST /providers/Microsoft.Portal/consoles/{consoleName}/keepAlive
-
     const session = await vscode.authentication.getSession('microsoft', ['https://management.core.windows.net//.default'], { silent: true });
     const accessToken = session?.accessToken;
 
-    if (accessToken) {
-        const keepAliveUrl = `${consoleUri}/keepAlive`;
-        const keepAliveHeaders = {
+    if (!accessToken) {
+        ext.outputChannel.error('Failed to get access token.');
+        return;
+    }
+    const response = await fetch(`${consoleUri}/keepAlive`, {
+        method: 'POST',
+        headers: {
             'Authorization': `Bearer ${accessToken}`,
             'Content-Type': 'application/json',
             'Accept': 'application/json',
-        };
+        },
+        body: JSON.stringify({}),
+    });
 
-        const response = await fetch(keepAliveUrl, {
-            method: 'POST',
-            headers: keepAliveHeaders,
-            body: JSON.stringify({}),
-        });
+    if (response.ok) {
+        ext.outputChannel.debug('Successfully called Cloud Shell keepAlive endpoint.');
+    } else {
+        ext.outputChannel.error('Failed to call Cloud Shell keepAlive endpoint.');
+        ext.outputChannel.error(response.statusText);
+        ext.outputChannel.error(await response.text());
+    }
+}
 
-        if (response.ok) {
-            ext.outputChannel.appendLine('Successfully called Cloud Shell keepAlive endpoint.');
-        } else {
-            ext.outputChannel.error('Failed to call Cloud Shell keepAlive endpoint.');
-            ext.outputChannel.error(response.statusText);
-            ext.outputChannel.error(await response.text());
-        }
+async function cloudShellSize(consoleUri: string, terminalId: string): Promise<void> {
+    const session = await vscode.authentication.getSession('microsoft', ['https://management.core.windows.net//.default'], { silent: true });
+    const accessToken = session?.accessToken;
+
+    if (!accessToken) {
+        ext.outputChannel.error('Failed to get access token.');
+        return;
+    }
+
+    const sizeResponse = await fetch(`${consoleUri}/terminals/${terminalId}/size?cols=68&rows=15&api-version=2019-01-01`, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({}),
+    });
+
+    if (sizeResponse.ok) {
+        ext.outputChannel.debug('Successfully called Cloud Shell size endpoint.');
+    } else {
+        ext.outputChannel.error('Failed to call Cloud Shell size endpoint.');
+        ext.outputChannel.error(sizeResponse.statusText);
+        ext.outputChannel.error(await sizeResponse.text());
     }
 }
