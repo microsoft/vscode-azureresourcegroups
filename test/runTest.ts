adcb34e8613fd949e82aa32ba07e4bf4272da9c9
alexweininger <alex.weininger@live.com>
2024-05-03 15:23:44 -0700

    Remove AA

--- test/runTest.ts
+++ test/runTest.ts
@@ -16,7 +16,6 @@ async function main(): Promise<void> {
             cli,
             [
                 ...args,
-                '--install-extension', 'ms-vscode.azure-account',
             ],
             {
                 encoding: 'utf-8',
