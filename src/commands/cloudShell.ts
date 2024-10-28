ff63b933354f8260b2d5c0382dd138d146bbdd1e
Alex Weininger <alex.weininger@live.com>
2024-10-28 14:40:26 -0700

    Add cloud shell telemetry (#945)
    * add /azure telemetry
    
    * Add log

--- src/commands/cloudShell.ts
+++ src/commands/cloudShell.ts
@@ -3,7 +3,7 @@
 *  Licensed under the MIT License. See License.txt in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
-import { IActionContext, parseError } from '@microsoft/vscode-azext-utils';
+import { callWithTelemetryAndErrorHandling, IActionContext, parseError } from '@microsoft/vscode-azext-utils';
 import { exec } from 'child_process';
 import * as vscode from 'vscode';
 import { ext } from '../extensionVariables';
@@ -21,9 +21,18 @@ function delay(ms: number) {
 interface MaintainCloudShellConnectionOptions {
     consoleUri: string;
     terminalId: string;
+    templateUrl?: string;
 }
 
 export async function maintainCloudShellConnection(_context: IActionContext, options: MaintainCloudShellConnectionOptions) {
+
+    try {
+        await recordTelemetry({ templateUrl: options.templateUrl });
+    } catch (e) {
+        // Don't crash if telemetry fails
+        ext.outputChannel.error(parseError(e).message);
+    }
+
     while (true) {
         try {
             await refreshDevTunnelAccessToken();
@@ -121,4 +130,12 @@ async function cloudShellSize(consoleUri: string, terminalId: string): Promise<v
         ext.outputChannel.error(sizeResponse.statusText);
         ext.outputChannel.error(await sizeResponse.text());
     }
+}
+
+async function recordTelemetry(options: { templateUrl?: string }) {
+    await callWithTelemetryAndErrorHandling('vscode-dev-azure.cloudShellConnection', async (context) => {
+        if (options.templateUrl) {
+            context.telemetry.properties.templateUrl = options.templateUrl;
+        }
+    });
 }
