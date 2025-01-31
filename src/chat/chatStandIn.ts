267c7906e781da7b96d4bcb56e53309a0976b38d
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-01-31 13:50:06 -0500

    Flip the boolean and add a wrapper command

--- src/chat/chatStandIn.ts
+++ src/chat/chatStandIn.ts
@@ -3,17 +3,17 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { callWithTelemetryAndErrorHandling, IActionContext } from '@microsoft/vscode-azext-utils';
+import { callWithTelemetryAndErrorHandling, IActionContext, registerCommand } from '@microsoft/vscode-azext-utils';
 import * as vscode from 'vscode';
 
 const GitHubCopilotForAzureExtensionId = 'ms-azuretools.vscode-azure-github-copilot';
 
 export function registerChatStandInParticipantIfNeeded(context: vscode.ExtensionContext): void {
     const ghcp4aInstalled = vscode.extensions.getExtension(GitHubCopilotForAzureExtensionId) !== undefined;
-    const disableChatStandIn = vscode.workspace.getConfiguration('azureResourceGroups').get<boolean | undefined>('disableChatStandIn');
+    const enableChatStandIn = vscode.workspace.getConfiguration('azureResourceGroups').get<boolean | undefined>('enableChatStandIn');
 
     if (ghcp4aInstalled || // If the GitHub Copilot for Azure extension is already installed, don't register the chat participant
-        disableChatStandIn === true) { // If the user has disabled chat standin by any means, don't register the chat participant
+        !enableChatStandIn) { // If the user has disabled chat standin by any means, don't register the chat participant
         return;
     }
 
@@ -24,6 +24,7 @@ export function registerChatStandInParticipantIfNeeded(context: vscode.Extension
     chatStandInParticipant.iconPath = vscode.Uri.joinPath(context.extensionUri, 'resources', 'GitHubCopilotforAzure.svg');
 
     context.subscriptions.push(chatStandInParticipant);
+    registerCommand('azureResourcesGroups.installGitHubCopilotForAzure', installGitHubCopilotForAzure);
 }
 
 async function chatStandIn(
@@ -36,15 +37,17 @@ async function chatStandIn(
     responseStream.markdown(vscode.l10n.t('I can help with that. First, you need to install the GitHub Copilot for Azure extension.'));
     responseStream.button({
         title: vscode.l10n.t('Install GitHub Copilot for Azure'),
-        command: 'workbench.extensions.installExtension',
-        arguments: [GitHubCopilotForAzureExtensionId],
+        command: 'azureResourcesGroups.installGitHubCopilotForAzure',
     });
 
-    responseStream.markdown(vscode.l10n.t('Next, you need to restart Visual Studio Code.'));
-    responseStream.button({
-        title: vscode.l10n.t('Restart Visual Studio Code'),
-        command: 'workbench.action.reloadWindow',
-    });
+    responseStream.markdown(vscode.l10n.t('After that, please repeat your question.'));
+}
 
-    responseStream.markdown(vscode.l10n.t('Finally, please repeat your question.'));
+async function installGitHubCopilotForAzure(context: IActionContext): Promise<void> {
+    try {
+        await vscode.commands.executeCommand('workbench.extensions.installExtension', GitHubCopilotForAzureExtensionId);
+    } catch (error) {
+        // Almost certainly the user cancelled the installation
+        context.telemetry.properties.result = 'Canceled';
+    }
 }
