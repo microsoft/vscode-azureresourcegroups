84496d629e1a56259b7bab6b826adb2f07182cfd
Chunan Ye <chuye@microsoft.com>
2025-02-06 14:14:23 -0800

    Polish comments

--- src/chat/askAgentAboutResource.ts
+++ src/chat/askAgentAboutResource.ts
@@ -10,14 +10,14 @@ const agentName = "azure";
 
 /**
  * A stand-in implementation of the Ask Azure command that should be invoked when the GitHub Copilot for Azure extension is not installed.
- * This implementation should inform the standIn chat provider to guide the user to reuse the Ask Azure action after installing the GitHub Copilot for Azure extension.
+ * This implementation should inform the stand-in chat provider to guide the user to reuse the Ask Azure action after installing the GitHub Copilot for Azure extension.
  */
 export async function askAgentAboutResource(_actionContext: IActionContext, node?: { id?: string, value?: { armId: string }, resource?: { kind?: string } }) {
     const resourceId = node?.id ?? node?.value?.armId;
     if (resourceId !== undefined) {
         const prompt = `@${agentName} /setResourceContext I would like to talk to a selected resource.`;
 
-        // GitHub Copilot for Azure needs to set the resourceConnect object to its in-memory cache but we cannot do it here because that extension is not installed.
+        // GitHub Copilot for Azure needs to set the resourceConnection object to its in-memory cache, but we cannot do it here because that extension is not installed.
         // So we submit a prompt indicating the intention of the Ask Azure action and guide the user on the next steps.
         await vscode.commands.executeCommand("workbench.action.chat.newChat");
         await vscode.commands.executeCommand("workbench.action.chat.open", { query: prompt });
