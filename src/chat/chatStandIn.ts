f463267f7727b598296e0dca39c7397e23d5acbc
Matthew Rayermann <marayerm@microsoft.com>
2025-02-07 14:52:49 -0800

    Do exact prompt match

--- src/chat/chatStandIn.ts
+++ src/chat/chatStandIn.ts
@@ -5,6 +5,7 @@
 
 import { callWithTelemetryAndErrorHandling, IActionContext, registerCommand } from '@microsoft/vscode-azext-utils';
 import * as vscode from 'vscode';
+import { askAgentAboutResourcePrompt } from './askAgentAboutResource';
 
 const GitHubCopilotForAzureExtensionId = 'ms-azuretools.vscode-azure-github-copilot';
 
@@ -40,11 +41,9 @@ async function chatStandIn(
         command: 'azureResourcesGroups.installGitHubCopilotForAzureFromChat',
     });
 
-    if (request.prompt.startsWith("/setResourceContext ")) {
-        responseStream.markdown(vscode.l10n.t(`After that, please use \`Ask @azure\` again to set your resource context.`));
-    } else {
-        responseStream.markdown(vscode.l10n.t('After that, please repeat your question.'));
-    }
+    const postButtonMessage = request.prompt === askAgentAboutResourcePrompt ? vscode.l10n.t(`After that, please use \`Ask @azure\` again.`) :
+        vscode.l10n.t('After that, please repeat your question.');
+    responseStream.markdown(postButtonMessage);
 }
 
 async function installGitHubCopilotForAzureFromChat(context: IActionContext): Promise<void> {
