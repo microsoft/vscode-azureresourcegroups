48ee43481b36a944487e6cfa2fc57f58ed622b5e
Tomasz Naumowicz <tnaumowicz@microsoft.com>
2025-04-01 20:10:16 +0200

    Removed obsolete `mongoClusters` feature flag (#1105)
    The feature has been fully shipped in Azure Databases VS Code Extension, the development-stage feature flag is no longer required.

--- src/azureExtensions.ts
+++ src/azureExtensions.ts
@@ -4,31 +4,8 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { AppResource } from "@microsoft/vscode-azext-utils/hostapi";
-import { extensions } from "vscode";
 import { AzExtResourceType } from "../api/src/index";
 import { localize } from "./utils/localize";
-
-
-/**
- * This is a temporary function usend to optionaly enable support for MongoClusters in the Azure Resources extension.
- *
- * This solution is necessary for a staged release of the MongoClusters feature from the vscode-cosmosdb extension.
- * It will be removed once the MongoClusters feature is fully released.
- *
- * @returns
- */
-function enableMongoClustersSupport() {
-    const vsCodeCosmosDBConfiguration = extensions.getExtension('ms-azuretools.vscode-cosmosdb')?.packageJSON as ExtensionPackageMongoClustersEnabled;
-    return (vsCodeCosmosDBConfiguration && vsCodeCosmosDBConfiguration.enableMongoClusters);
-}
-
-/**
- * This is a temporary interface used to enable support for MongoClusters in the Azure Resources extension.
- * It will be removed once the MongoClusters feature is fully released.
- */
-interface ExtensionPackageMongoClustersEnabled {
-    readonly enableMongoClusters?: boolean;
-}
 
 export const azureExtensions: IAzExtMetadata[] = [
     {
@@ -101,23 +78,12 @@ export const azureExtensions: IAzExtMetadata[] = [
     {
         name: 'vscode-cosmosdb',
         label: 'Databases',
-        resourceTypes:
-            /**
-            * This is a temporary interface used to enable support for MongoClusters in the Azure Resources extension.
-            * It will be removed once the MongoClusters feature is fully released.
-            */
-            enableMongoClustersSupport() ?
-                [
-                    AzExtResourceType.AzureCosmosDb,
-                    AzExtResourceType.MongoClusters,
-                    AzExtResourceType.PostgresqlServersStandard,
-                    AzExtResourceType.PostgresqlServersFlexible,
-                ] :
-                [
-                    AzExtResourceType.AzureCosmosDb,
-                    AzExtResourceType.PostgresqlServersStandard,
-                    AzExtResourceType.PostgresqlServersFlexible,
-                ],
+        resourceTypes: [
+            AzExtResourceType.AzureCosmosDb,
+            AzExtResourceType.MongoClusters,
+            AzExtResourceType.PostgresqlServersStandard,
+            AzExtResourceType.PostgresqlServersFlexible,
+        ],
         reportIssueCommandId: 'azureDatabases.reportIssue'
     },
     {
