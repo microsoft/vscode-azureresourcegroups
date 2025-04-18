53e6d8cecf8e09a9844c97adaa709b7c134e4163
Nathan <naturins@microsoft.com>
2025-04-18 09:00:20 -0700

    Have a backwards compat shim for the activity log view (#1123)
    * Have a backwards compat shim for the activity log view
    
    * Revert back to none

--- src/tree/activityLog/ActivityLogBranchDataProvider.ts
+++ src/tree/activityLog/ActivityLogBranchDataProvider.ts
@@ -3,13 +3,15 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { callWithTelemetryAndErrorHandling, IActionContext, TreeElementBase } from '@microsoft/vscode-azext-utils';
+import { callWithTelemetryAndErrorHandling, IActionContext, isAzExtTreeItem, parseError, TreeElementBase } from '@microsoft/vscode-azext-utils';
 import { Activity } from '@microsoft/vscode-azext-utils/hostapi';
-import { commands, Event } from 'vscode';
+import { commands, Event, TreeItem } from 'vscode';
 import { ActivityLogResourceProviderManager } from '../../api/ResourceProviderManagers';
 import { ext } from '../../extensionVariables';
 import { settingUtils } from '../../utils/settingUtils';
 import { BranchDataItemCache } from '../BranchDataItemCache';
+import { InvalidItem } from '../InvalidItem';
+import { TreeDataItem } from '../ResourceGroupsItem';
 import { ResourceTreeDataProviderBase } from '../ResourceTreeDataProviderBase';
 import { WorkspaceResourceBranchDataProviderManager } from '../workspace/WorkspaceResourceBranchDataProviderManager';
 import { ActivityItem, ActivityStatus } from './ActivityItem';
@@ -33,6 +35,30 @@ export class ActivityLogTreeDataProvider extends ResourceTreeDataProviderBase {
             return await element.getChildren();
         } else {
             return Object.values(this.activityTreeItems).filter((activity) => !!activity.status);
+        }
+    }
+
+    async getTreeItem(element: TreeDataItem): Promise<TreeItem> {
+        try {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            return (await callWithTelemetryAndErrorHandling('getTreeItem', async (context) => {
+                context.errorHandling.rethrow = true;
+                // very basic v1 backwards compatibility
+                if (isAzExtTreeItem(element)) {
+                    return {
+                        label: element.label,
+                        iconPath: element.iconPath,
+                        contextValue: element.contextValue,
+                        description: element.description
+                    }
+                } else {
+                    return await element.getTreeItem();
+                }
+
+            }))!;
+        } catch (e) {
+            const invalidItem = new InvalidItem(parseError(e));
+            return invalidItem.getTreeItem();
         }
     }
 
