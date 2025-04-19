b674d65a8cc31b38b62678646b4d1635c86b40dd
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2025-04-18 21:23:14 -0700

    Update LM content

--- src/chat/tools/GetAzureActivityLog/GetAzureActivityLog.ts
+++ src/chat/tools/GetAzureActivityLog/GetAzureActivityLog.ts
@@ -15,14 +15,14 @@ export class GetAzureActivityLog<T extends GetAzureActivityLogInputSchema> imple
         return {
             content: [
                 convertedActivityItems.length ?
-                    generateContextualizedPrompt(JSON.stringify(convertedActivityItems), options.input.promptType ?? ActivityLogPromptType.Explain) :
+                    instructLanguageModelResponse(JSON.stringify(convertedActivityItems), options.input.promptType ?? ActivityLogPromptType.Explain) :
                     new vscode.LanguageModelTextPart('There is no activity data to analyze.')
             ]
         };
     }
 }
 
-function generateContextualizedPrompt(treeItemData: string, promptType: ActivityLogPromptType): vscode.LanguageModelTextPart {
+function instructLanguageModelResponse(treeItemData: string, promptType: ActivityLogPromptType): vscode.LanguageModelTextPart {
     switch (promptType) {
         case ActivityLogPromptType.Fix:
             return new vscode.LanguageModelTextPart(
