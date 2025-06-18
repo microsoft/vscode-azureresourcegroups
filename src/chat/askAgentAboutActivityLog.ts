9ec1da42cd5a6e1653c27db0c138efcb7fffb848
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2025-06-18 13:42:22 -0700

    Open the chat before calling newChat

--- src/chat/askAgentAboutActivityLog.ts
+++ src/chat/askAgentAboutActivityLog.ts
@@ -8,6 +8,7 @@ import * as vscode from "vscode";
 const genericActivityLogPrompt: string = vscode.l10n.t('Help explain important information from my Azure activity log.');
 
 export async function askAgentAboutActivityLog(): Promise<void> {
+    await vscode.commands.executeCommand("workbench.action.chat.open");
     await vscode.commands.executeCommand("workbench.action.chat.newChat");
     await vscode.commands.executeCommand("workbench.action.chat.open", { mode: 'agent', query: genericActivityLogPrompt });
 }
