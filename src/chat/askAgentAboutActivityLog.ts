17a04199a94d60257c3ae223cb61ecd13338241e
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2025-06-19 14:27:08 -0500

    Feedback

--- src/chat/askAgentAboutActivityLog.ts
+++ src/chat/askAgentAboutActivityLog.ts
@@ -8,6 +8,7 @@ import * as vscode from "vscode";
 const genericActivityLogPrompt: string = vscode.l10n.t('Help explain important information from my Azure activity log.');
 
 export async function askAgentAboutActivityLog(): Promise<void> {
+    // It appears you need to have the chat already open before you call `newChat`, otherwise it won't actually reset to a new dialogue
     await vscode.commands.executeCommand("workbench.action.chat.open");
     await vscode.commands.executeCommand("workbench.action.chat.newChat");
     await vscode.commands.executeCommand("workbench.action.chat.open", { mode: 'agent', query: genericActivityLogPrompt });
