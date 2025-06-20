7b46bd6dd31d838ff67508372ecbd10add0e8e3d
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2025-06-19 19:37:36 -0500

    Remove init

--- src/utils/activityUtils.ts
+++ src/utils/activityUtils.ts
@@ -6,6 +6,5 @@ export async function createActivityContext(): Promise<ExecuteActivityContext> {
     return {
         registerActivity: async (activity) => registerActivity(activity),
         suppressNotification: await settingUtils.getWorkspaceSetting('suppressActivityNotifications'),
-        activityAttributes: {},
     };
 }
