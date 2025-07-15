e91952b6d4867691a1bbad9bbd17f409854941eb
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2025-07-15 12:29:16 -0700

    More telemetry

--- src/chat/tools/GetAzureActivityLog/GetAzureActivityLog.ts
+++ src/chat/tools/GetAzureActivityLog/GetAzureActivityLog.ts
@@ -10,29 +10,7 @@ import { convertActivityTreeToSimpleObjectArray, ConvertedActivityItem } from '.
 export class GetAzureActivityLog implements AzExtLMTool<void> {
     public async invoke(context: IActionContext): Promise<vscode.LanguageModelToolResult> {
         const convertedActivityItems: ConvertedActivityItem[] = await convertActivityTreeToSimpleObjectArray(context);
-
-        type ReduceCounters = { commandIds: string[]; failedCommandIds: string[], totalFailedActivities: number };
-        const { commandIds, failedCommandIds, totalFailedActivities } = convertedActivityItems.reduce<ReduceCounters>((counter, activityItem) => {
-            if (activityItem.error) {
-                counter.totalFailedActivities++;
-            }
-
-            if (activityItem.commandId) {
-                counter.commandIds.push(activityItem.commandId);
-
-                if (activityItem.error) {
-                    counter.failedCommandIds.push(activityItem.commandId);
-                }
-            }
-
-            return counter;
-        }, { commandIds: [], failedCommandIds: [], totalFailedActivities: 0 });
-
-        context.telemetry.properties.activityCount = String(convertedActivityItems.length);
-        context.telemetry.properties.failedActivityCount = String(totalFailedActivities);
-
-        context.telemetry.properties.commandIds = commandIds.join(',');
-        context.telemetry.properties.failedCommandIds = failedCommandIds.join(',');
+        logTelemetry(context, convertedActivityItems);
 
         if (convertedActivityItems.length === 0) {
             return new vscode.LanguageModelToolResult([new vscode.LanguageModelTextPart('No activity log items found.')]);
@@ -40,4 +18,53 @@ export class GetAzureActivityLog implements AzExtLMTool<void> {
 
         return new vscode.LanguageModelToolResult(convertedActivityItems.map(item => new vscode.LanguageModelTextPart(JSON.stringify(item))));
     }
+}
+
+type TelemetryCounters = {
+    commandIds: string[];
+    failedCommandIds: string[];
+    commandIdsWithAttributes: string[];
+    failedCommandIdsWithAttributes: string[];
+    totalFailedActivities: number;
+};
+
+function logTelemetry(context: IActionContext, convertedActivityItems: ConvertedActivityItem[]) {
+    const telemetry: TelemetryCounters = convertedActivityItems.reduce<TelemetryCounters>((telemetry, activityItem) => {
+        if (activityItem.error) {
+            telemetry.totalFailedActivities++;
+        }
+
+        if (activityItem.commandId) {
+            telemetry.commandIds.push(activityItem.commandId);
+            if (activityItem.activityAttributes) {
+                telemetry.commandIdsWithAttributes.push(activityItem.commandId);
+            }
+
+
+            if (activityItem.error) {
+                telemetry.failedCommandIds.push(activityItem.commandId);
+                if (activityItem.activityAttributes) {
+                    telemetry.failedCommandIdsWithAttributes.push(activityItem.commandId);
+                }
+            }
+        }
+
+        return telemetry;
+
+    }, {
+        commandIds: [],
+        failedCommandIds: [],
+        commandIdsWithAttributes: [],
+        failedCommandIdsWithAttributes: [],
+        totalFailedActivities: 0,
+    });
+
+    context.telemetry.properties.activityCount = String(convertedActivityItems.length);
+    context.telemetry.properties.failedActivityCount = String(telemetry.totalFailedActivities);
+
+    context.telemetry.properties.commandIds = telemetry.commandIds.join(',');
+    context.telemetry.properties.failedCommandIds = telemetry.failedCommandIds.join(',');
+
+    context.telemetry.properties.commandIdsWithAttributes = telemetry.commandIdsWithAttributes.join(',');
+    context.telemetry.properties.failedCommandIdsWithAttributes = telemetry.failedCommandIdsWithAttributes.join(',');
 }
