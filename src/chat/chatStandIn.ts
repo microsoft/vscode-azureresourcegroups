26b26c7e84bf6ab4df686445a1e5b5d930f12678
Megan Mott <meganmott@microsoft.com>
2025-10-02 09:50:16 -0700

    button to disable setting

--- src/chat/chatStandIn.ts
+++ src/chat/chatStandIn.ts
@@ -5,6 +5,7 @@
 
 import { callWithTelemetryAndErrorHandling, IActionContext, registerCommand } from '@microsoft/vscode-azext-utils';
 import * as vscode from 'vscode';
+import { settingUtils } from '../utils/settingUtils';
 import { askAgentAboutResourcePrompt } from './askAgentAboutResource';
 
 const GitHubCopilotForAzureExtensionId = 'ms-azuretools.vscode-azure-github-copilot';
@@ -26,6 +27,7 @@ export function registerChatStandInParticipantIfNeeded(context: vscode.Extension
 
     context.subscriptions.push(chatStandInParticipant);
     registerCommand('azureResourcesGroups.installGitHubCopilotForAzureFromChat', installGitHubCopilotForAzureFromChat);
+    registerCommand('azureResourceGroups.updateChatStandInSetting', updateChatStandInSetting);
 }
 
 async function chatStandIn(
@@ -42,8 +44,15 @@ async function chatStandIn(
     });
 
     const postButtonMessage = request.prompt === askAgentAboutResourcePrompt ? vscode.l10n.t(`After that, please use \`Ask @azure\` again.`) :
-        vscode.l10n.t('After that, please repeat your question.');
+        vscode.l10n.t(`After that, please repeat your question.\n\n`);
     responseStream.markdown(postButtonMessage);
+
+    responseStream.markdown(vscode.l10n.t('Or you can also disable this message by clicking the button below.'));
+
+    responseStream.button({
+        title: vscode.l10n.t(`Don't ask me again`),
+        command: 'azureResourceGroups.updateChatStandInSetting'
+    });
 }
 
 async function installGitHubCopilotForAzureFromChat(context: IActionContext): Promise<void> {
@@ -53,4 +62,8 @@ async function installGitHubCopilotForAzureFromChat(context: IActionContext): Pr
         // Almost certainly the user cancelled the installation
         context.telemetry.properties.result = 'Canceled';
     }
+}
+
+async function updateChatStandInSetting(): Promise<void> {
+    await settingUtils.updateGlobalSetting('enableChatStandIn', false);
 }
