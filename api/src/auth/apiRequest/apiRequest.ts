5887d12ac26f94525d2ff7490224bccc9d07e4f8
Matthew Fisher <40250218+MicroFish91@users.noreply.github.com>
2026-01-23 13:27:27 -0800

    Add Azure Resources API (v4) client tooling (#1313)

---
+++ api/src/auth/apiRequest/apiRequest.ts
@@ -0,0 +1,114 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { l10n } from "vscode";
+import { AzureResourcesExtensionAuthApi } from "../../extensionApi";
+import { apiUtils, AzureExtensionApi } from "../../utils/apiUtils";
+import { AzExtCredentialManager } from "../credentialManager/AzExtCredentialManager";
+import { AzExtUUIDCredentialManager } from "../credentialManager/AzExtUUIDCredentialManager";
+import { AzureResourcesApiRequestContext } from "./AzureResourcesApiRequestContext";
+import { AzureResourcesApiRequestErrorCode } from "./apiRequestErrors";
+
+const azureResourcesAuthApiVersion: string = '^4.0.0';
+const azureResourcesExtId = 'ms-azuretools.vscode-azureresourcegroups';
+
+export type AzureResourcesApiRequestPrep<T extends AzureExtensionApi> = {
+    /**
+     * The modified client extension API.  Ensures the required handshake receiver method has been added.
+     */
+    clientApi: T & Required<Pick<T, 'receiveAzureResourcesApiSession'>>;
+
+    /**
+     * Initiates the authentication handshake required to obtain the Azure Resources API.
+     */
+    requestResourcesApis: () => void;
+};
+
+/**
+ * Prepares a client extension for the Azure Resources authentication handshake.
+ *
+ * @param context - Prerequisite configuration and handlers to prepare the request
+ * @param clientExtensionApi - The base extension API to be modified
+ * @returns The modified client extension API (with the required receiver method added), and a method to initiate the handshake
+ */
+export function prepareAzureResourcesApiRequest<T extends AzureExtensionApi>(context: AzureResourcesApiRequestContext, clientExtensionApi: T): AzureResourcesApiRequestPrep<T> {
+    if (!context.azureResourcesApiVersions.length) {
+        throw new Error('You must specify at least one Azure Resources API version.');
+    }
+
+    const clientCredentialManager: AzExtCredentialManager = new AzExtUUIDCredentialManager();
+
+    if (!clientExtensionApi.receiveAzureResourcesApiSession) {
+        clientExtensionApi.receiveAzureResourcesApiSession = createReceiveAzureResourcesApiSession(context, clientCredentialManager);
+    }
+
+    return {
+        clientApi: clientExtensionApi as T & Required<Pick<T, 'receiveAzureResourcesApiSession'>>,
+        requestResourcesApis: () => void requestAzureResourcesSession(context, clientCredentialManager, clientExtensionApi.apiVersion),
+    };
+}
+
+async function requestAzureResourcesSession(context: AzureResourcesApiRequestContext, clientCredentialManager: AzExtCredentialManager, clientApiVersion: string): Promise<void> {
+    let clientCredential: string;
+    try {
+        clientCredential = await clientCredentialManager.createCredential(context.clientExtensionId);
+    } catch (err) {
+        if (err instanceof Error) {
+            void context.onApiRequestError?.({ code: AzureResourcesApiRequestErrorCode.ClientFailedCreateCredential, message: clientCredentialManager.maskCredentials(err.message) });
+        }
+        return;
+    }
+
+    try {
+        const resourcesAuthApi = await getExtensionApi<AzureResourcesExtensionAuthApi>(azureResourcesExtId, azureResourcesAuthApiVersion);
+        await resourcesAuthApi.createAzureResourcesApiSession(context.clientExtensionId, clientApiVersion, clientCredential);
+    } catch (err) {
+        if (err instanceof Error) {
+            void context.onApiRequestError?.({ code: AzureResourcesApiRequestErrorCode.HostCreateSessionFailed, message: clientCredentialManager.maskCredentials(err.message) });
+        }
+        return;
+    }
+}
+
+function createReceiveAzureResourcesApiSession(context: AzureResourcesApiRequestContext, clientCredentialManager: AzExtCredentialManager): AzureExtensionApi['receiveAzureResourcesApiSession'] {
+    return async function (azureResourcesCredential: string, clientCredential: string): Promise<void> {
+        if (!azureResourcesCredential || !clientCredential) {
+            void context.onApiRequestError?.({ code: AzureResourcesApiRequestErrorCode.ClientReceivedInsufficientCredentials, message: 'Insufficient credentials were provided back to the client.' });
+            return;
+        }
+
+        try {
+            const verified = await clientCredentialManager.verifyCredential(clientCredential, context.clientExtensionId);
+            if (!verified) {
+                throw new Error('Client credential returned did not pass verification.');
+            }
+        } catch (err) {
+            if (err instanceof Error) {
+                void context.onApiRequestError?.({ code: AzureResourcesApiRequestErrorCode.ClientCredentialFailedVerification, message: clientCredentialManager.maskCredentials(err.message) });
+            }
+            return;
+        }
+
+        try {
+            const resourcesAuthApi = await getExtensionApi<AzureResourcesExtensionAuthApi>(azureResourcesExtId, azureResourcesAuthApiVersion);
+            const resourcesApis = await resourcesAuthApi.getAzureResourcesApis(context.clientExtensionId, azureResourcesCredential, context.azureResourcesApiVersions);
+            void context.onDidReceiveAzureResourcesApis(resourcesApis);
+        } catch (err) {
+            if (err instanceof Error) {
+                void context.onApiRequestError?.({ code: AzureResourcesApiRequestErrorCode.HostApiProvisioningFailed, message: clientCredentialManager.maskCredentials(err.message) });
+            }
+            return;
+        }
+    };
+}
+
+async function getExtensionApi<T extends AzureExtensionApi>(extensionId: string, extensionVersion: string): Promise<T> {
+    const extensionProvider = await apiUtils.getExtensionExports<apiUtils.AzureExtensionApiProvider>(extensionId);
+    if (extensionProvider) {
+        return extensionProvider.getApi<T>(extensionVersion);
+    } else {
+        throw new Error(l10n.t('Could not find Azure extension API for extension "{0}".', extensionId));
+    }
+}
---
+++ api/src/auth/apiRequest/apiRequestErrors.ts
@@ -0,0 +1,45 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+/**
+ * Codes for errors that could appear during the API request handshake between client extension and Azure Resources host extension.
+ */
+export enum AzureResourcesApiRequestErrorCode {
+    /**
+     * An error occurred while the client extension was creating its verification credential for the Azure Resources host extension.
+     */
+    ClientFailedCreateCredential = 'ERR_CLIENT_FAILED_CREATE_CREDENTIAL',
+
+    /**
+     * An error occurred while the Azure Resources host extension was trying to create an API session.
+     */
+    HostCreateSessionFailed = 'ERR_HOST_CREATE_SESSION_FAILED',
+
+    /**
+     * An error occurred because the client's receiver method was provided incomplete or missing credentials.
+     */
+    ClientReceivedInsufficientCredentials = 'ERR_CLIENT_RECEIVED_INSUFFICIENT_CREDENTIALS',
+
+    /**
+     * The client's receiver method was provided a client credential that failed verification.
+     *
+     * This may occur when:
+     * - An untrusted extension pretends to be the Azure Resources host extension and tries to pass a fake credential
+     * - There is a faulty behavior in the client's verification process
+     */
+    ClientCredentialFailedVerification = 'ERR_CLIENT_CREDENTIAL_FAILED_VERIFICATION',
+
+    /**
+     * An error occurred while asking the Azure Resources host extension to provision the specified APIs.
+     *
+     * This may occur when:
+     * - The Azure Resources extension cannot verify the issued credential that was passed back
+     * - The requesting extension is not on the Azure Resources allow list
+     * - The host extension encounters an internal error during API provisioning
+     */
+    HostApiProvisioningFailed = 'ERR_HOST_API_PROVISIONING_FAILED',
+}
+
+export type AzureResourcesApiRequestError = { code: AzureResourcesApiRequestErrorCode, message: string };
