0e4addd006ede11bddeb60a4881ef8a3d215d72c
Nathan Turinski <naturins@microsoft.comm>
2026-02-17 14:05:53 -0800

    Skeptics be damned

--- src/commands/accounts/logIn.ts
+++ src/commands/accounts/logIn.ts
@@ -11,7 +11,9 @@ let _isLoggingIn: boolean = false;
 
 export async function logIn(_context: IActionContext): Promise<void> {
     try {
-        const provider = await ext.subscriptionProviderFactory();
+        const provider = ext.testing.overrideAzureSubscriptionProvider
+            ? ext.testing.overrideAzureSubscriptionProvider()
+            : await ext.subscriptionProviderFactory();
         _isLoggingIn = true;
         ext.actions.refreshAzureTree(); // Refresh to cause the "logging in" spinner to show
         ext.actions.refreshTenantTree(); // Refresh to cause the "logging in" spinner to show
