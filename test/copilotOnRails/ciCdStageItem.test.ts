bfffa48761245626cb19ce5895258df6f1b4cf94
Megan Mott <meganmott@Megans-MacBook-Pro.local>
2026-09-22 10:10:34 -0700

    initial

---
+++ test/copilotOnRails/ciCdStageItem.test.ts
@@ -0,0 +1,26 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import assert from 'assert';
+import * as vscode from 'vscode';
+import { CiCdStageItem, ciCdSurveyUrl } from '../../src/tree/project/CiCdStageItem';
+
+suite('CiCdStageItem', () => {
+    test('shows the CI/CD stage with a survey action', () => {
+        const stage = new CiCdStageItem(2);
+        const stageItem = stage.getTreeItem();
+
+        assert.strictEqual(stageItem.label, '4. CI/CD');
+        assert.strictEqual(stageItem.description, 'Coming soon');
+
+        const children = stage.getChildren();
+        assert.strictEqual(children.length, 1);
+
+        const surveyItem = children[0].getTreeItem();
+        assert.strictEqual(surveyItem.label, 'Take survey');
+        assert.strictEqual(surveyItem.command?.command, 'vscode.open');
+        assert.strictEqual((surveyItem.command?.arguments?.[0] as vscode.Uri).toString(), ciCdSurveyUrl);
+    });
+});
